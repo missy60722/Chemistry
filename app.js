af3e158e95c59de98f65ddbf488f9d5e9b9f997d
w0417 <136731774+w0417@users.noreply.github.com>
2023-08-01 17:34:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// 開場動畫
+const lastWord = document.querySelector("#logo");
+const animation = document.querySelector("div.animation");
+lastWord.addEventListener("animationend", () => {
+  animation.style =
+    "transition: all 5s; opacity: 0; pointer-events: none; transition-delay: 1.5s";
+});
+
 function allowDrop(ev) {
   ev.preventDefault();
   // 這是要配合 HTML 的 ondragover 一起使用，目的是要移除元素預設行為
