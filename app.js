597bf4c8798d753d7672339ec1a9e5a2ab46bf68
missy60722 <81450774+missy60722@users.noreply.github.com>
2023-08-09 20:51:29 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,17 +6,49 @@ lastWord.addEventListener("animationend", () => {
     "transition: all 5s; opacity: 0; pointer-events: none; transition-delay: 1.5s";
 });
 
-function allowDrop(ev) {
-  ev.preventDefault();
-  // 這是要配合 HTML 的 ondragover 一起使用，目的是要移除元素預設行為
+//拖曳＆放置
+let source = null;
+
+const element = document.querySelectorAll(".element");
+for (let i = 0; i < element.length; i++) {
+  element[i].addEventListener("drag", (event) => {
+    event.dataTransfer.setData("Text", event.target.id);
+  });
+  element[i].addEventListener("dragstart", (event) => {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.classList.add("dragging");
+    source = event.target;
+  });
+  element[i].addEventListener("dragend", (event) => {
+    // reset the transparency
+    event.target.classList.remove("dragging");
+    source = null;
+  });
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData("Text", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault(); // 移除預設行為
-  var data = ev.dataTransfer.getData("Text"); // 存取資料
-  ev.target.appendChild(document.getElementById(data));
+const target = document.querySelectorAll(".target");
+for (let i = 0; i < target.length; i++) {
+  target[i].addEventListener("drag", (event) => {
+    event.dataTransfer.setData("Text", event.target.id);
+  });
+  target[i].addEventListener("dragenter", (event) => {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.classList.add("hover");
+  });
+  target[i].addEventListener("dragleave", (event) => {
+    // reset the transparency
+    event.target.classList.remove("hover");
+  });
+  target[i].addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
+  target[i].addEventListener("drop", (event) => {
+    event.target.appendChild(source);
+    event.target.classList.remove("hover");
+    event.stopImmediatePropagation();
+  });
 }
