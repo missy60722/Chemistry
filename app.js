d5653e2df2d7d35ec1d6bee5e1baa7a626b1b330
missy60722 <1missy60722@gmail.com>
2023-09-05 13:01:28 +0800

    2023.09.05 upgrade

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // 開場動畫
-const lastWord = document.querySelector("#logo");
+/*const lastWord = document.querySelector("#logo");
 const animation = document.querySelector("div.animation");
 lastWord.addEventListener("animationend", () => {
   animation.style =
     "transition: all 5s; opacity: 0; pointer-events: none; transition-delay: 1.5s";
-});
+});*/
 
 //拖曳＆放置
 let source = null;
@@ -29,6 +29,9 @@ for (let i = 0; i < element.length; i++) {
 }
 
 let videoReplay = document.querySelector(".replay");
+videoReplay.style.cursor = "not-allowed";
+videoReplay.disabled = true;
+let gameReload = document.querySelector(".return");
 const video = document.createElement("video");
 const box3 = document.querySelector("#box3");
 const target = document.querySelectorAll(".target");
@@ -41,25 +44,39 @@ for (let i = 0; i < target.length; i++) {
     dragged = event.target;
     // make it half transparent
     event.target.classList.add("hover");
+    if (source.classList.contains("answer") && event.target.classList.contains("droppable")) {
+      event.target.classList.add("answer");
+    }
+    else{
+      event.target.classList.add("not");
+    }
   });
   target[i].addEventListener("dragleave", (event) => {
     // reset the transparency
     event.target.classList.remove("hover");
+    if (source.classList.contains("answer") && event.target.classList.contains("droppable")) {
+      event.target.classList.remove("answer");
+    }
+    else{
+      event.target.classList.remove("not");
+    }
   });
   target[i].addEventListener("dragover", (event) => {
     event.preventDefault();
   });
   target[i].addEventListener("drop", (event) => {
-    console.log(source.parent);
-    event.target.appendChild(source);
+    event.preventDefault();
     event.target.classList.remove("hover");
-    if (source.classList.contains("answer")) {
+    
+    if (source.classList.contains("answer") && event.target.classList.contains("droppable")) {
+      videoReplay.style.cursor = "pointer";
+      videoReplay.disabled = false;
       video.src = "./pictures/co2.mp4";
       video.width = 400;
       video.autoplay = true;
       box3.appendChild(video);
       // videoStart.style = "display:block";
-      videoReplay.style = "opacity:1";
+      event.target.appendChild(source);
     }
     event.stopImmediatePropagation();
   });
@@ -67,6 +84,76 @@ for (let i = 0; i < target.length; i++) {
 
 // video replay
 videoReplay.addEventListener("click", (event) => {
-  event.preventDefault();
+  video.currentTime = 0;
   video.play();
 });
+
+const level = document.querySelectorAll(".level");
+level.forEach((span) => {
+  const targetId = span.getAttribute("data-target");
+  const targetElement = document.getElementById(targetId);
+  
+  if (targetElement) {
+    span.textContent = targetElement.textContent;
+  }
+});
+
+
+// 獲取所有的部分元素
+const sections = document.querySelectorAll("section");
+
+let currentSectionIndex = 0;
+sections[currentSectionIndex].classList.add("active");
+
+currentSectionIndex = sessionStorage.getItem("currentSectionIndex");
+
+// 如果有保存的索引，切换到对应的部分
+if (currentSectionIndex !== null) {
+  switchSection(parseInt(currentSectionIndex));
+}
+
+// 处理部分切换的函数
+function switchSection(sectionIndex) {
+  // 隐藏所有部分
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+
+  // 显示指定索引的部分
+  if (sectionIndex >= 0 && sectionIndex < sections.length) {
+    sections[sectionIndex].classList.add("active");
+    currentSectionIndex = sectionIndex;
+    sessionStorage.setItem("currentSectionIndex", currentSectionIndex);
+  }
+}
+
+// 切换到上一个部分
+const previous = document.querySelector(".previous");
+previous.addEventListener("click", () => {
+  const prevSectionIndex = currentSectionIndex - 1;
+  if (prevSectionIndex >= 0){
+    switchSection(prevSectionIndex);
+  }
+  
+});
+
+// 切换到下一个部分
+const next = document.querySelector(".next");
+next.addEventListener("click", () => {
+  const nextSectionIndex = currentSectionIndex + 1;
+  if (nextSectionIndex < sections.length){
+    switchSection(nextSectionIndex);
+  }
+});
+
+//重新整理頁面
+const reload = document.querySelector(".return");
+reload.addEventListener("click", () => {
+  location.reload();
+  const currentSectionIndex = sessionStorage.getItem("currentSectionIndex");
+
+  if (currentSectionIndex !== null) {
+    switchSection(parseInt(currentSectionIndex));
+  }
+});
+
